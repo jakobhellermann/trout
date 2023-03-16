ec3762e855959c62f81ea868fdb63632e8f47b6c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-03-16 21:40:29 +0100

    add ability to solve multiple

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ static ALLOC: dhat::Alloc = dhat::Alloc;
 
 mod solver;
 
+use std::path::PathBuf;
+
 use anyhow::{anyhow, Result};
 
 type Length = u32;
@@ -47,12 +49,7 @@ impl std::fmt::Debug for Node {
     }
 }
 
-fn main() -> Result<()> {
-    let path = std::env::args()
-        .nth(1)
-        .ok_or_else(|| anyhow!("missing first argument"))?;
-    let table = std::fs::read_to_string(&path)?;
-
+fn solve_table(table: &str) -> Result<()> {
     let table = parse_table(&table)?;
 
     let start = std::time::Instant::now();
@@ -91,6 +88,21 @@ fn main() -> Result<()> {
     println!("Routing took {:02}s", duration.as_secs_f32());
     println!("{} solutions", n_solutions);
     println!("Pathfind function called {} times.", stats.iterations);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let paths: Vec<_> = std::env::args().skip(1).map(PathBuf::from).collect();
+    anyhow::ensure!(paths.len() > 0, "missing argument of path to table");
+
+    for path in paths {
+        println!("Solving {}...", path.display());
+
+        let table = std::fs::read_to_string(&path)?;
+        solve_table(&table)?;
+        println!();
+    }
 
     Ok(())
 }
